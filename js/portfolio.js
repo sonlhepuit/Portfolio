49878587d307a8753715d1b6577529d77b1771cc
Lê Hồng Sơn <bapcaixanh009@gmail.com>
2020-04-05 19:54:12 +0700

    chờ đợi một người dù biết chẳng có cơ hội

---
+++ js/portfolio.js
@@ -0,0 +1,10 @@
+function openNav() {
+    document.getElementById("mySidebar").style.right = "0";
+    // document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("mySidebar").style.opacity = "1";
+  }
+  function closeNav() {
+    document.getElementById("mySidebar").style.right = "-100%";
+    // document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("mySidebar").style.opacity = "0";
+  }
