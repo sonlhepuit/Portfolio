53e7ebe1a989bc5630b9a737098213387aefac0a
Lê Hồng Sơn <bapcaixanh009@gmail.com>
2020-04-07 09:32:42 +0700

    Yêu Nhung 5000

--- js/portfolio.js
+++ js/portfolio.js
@@ -8,3 +8,27 @@ function openNav() {
     // document.getElementById("main").style.marginLeft = "250px";
     document.getElementById("mySidebar").style.opacity = "0";
   }
+
+
+  window.onscroll = function() {
+    scrollFunction()};
+  
+  function scrollFunction() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+      // document.getElementById("navbar").style.backgroundColor = "red";
+      // document.getElementById(navbar).style.height="150px";
+      document.getElementById("navbar").style.position="fixed ";
+      document.getElementById("navbar").style.top="0px";
+      document.getElementById("navbar").style.left="0px";
+      document.getElementById("navbar").style.right="0px";
+      document.getElementById("ctn").style.padding="0px";
+      document.getElementById("navbar").style.borderBottom="1px solid #d1d1d1";
+    } else {
+      document.getElementById("navbar").style.backgroundColor = "#f5f5f5";
+      document.getElementById("navbar").style.position="relative ";
+      document.getElementById("navbar").style.borderBottom="1px solid #f5f5f5";
+
+      // document.getElementById(navbar).style.height="100px";
+      // document.getElementsByClassName("nav-hidden-item").style.marginTop = "50px";
+    }
+  }
